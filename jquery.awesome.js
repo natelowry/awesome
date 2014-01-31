592fdd0ebcbfaa233b482e28e36e9e841c697475
Nate Lowry <nate.lowry@gmail.com>
2014-01-31 13:41:38 -0600

    Initial Commit

---
+++ jquery.awesome.js
@@ -0,0 +1,12 @@
+(function( $ ) {
+		'use strict'
+		 var awesomes = ["neat", "cool", "wicked", "incredible", "excellent",
+		 "crack", "exceptional", "fantastic", "fabulous", "fine", "awesome",
+		 "marvelous", "super", "terrific", "tremendous", "wonderful",
+		 "brilliant", "superb", "keen", "laudable", "worthy", "rad", "sweet",
+		 "fresh", "dope", "fly", "nifty", "first-rate", "impressive"];
+ 
+    $.fn.awesome = function() { 
+        return this.text(awesomes[parseInt(Math.random() * awesomes.length)]);         
+    }; 
+}( jQuery ));
